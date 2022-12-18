d42a622d87a33f81fa6166a043c8c642cd98b1ac
Isaac <maamouche.isaac@gmail.com>
2022-12-18 18:05:34 +0100

    feat: create footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,8 +1,13 @@
 import React from "react";
+
+import "./footer.scss";
+import logo from "../../images/logo.svg";
+
 export const Footer: React.FC = () => {
   return (
-    <>
-      <div>footer</div>
-    </>
+    <footer>
+      <img alt="" className="footer__img" src={logo} />
+      <p>© 2020 Kasa. All rights reserved</p>
+    </footer>
   );
 };

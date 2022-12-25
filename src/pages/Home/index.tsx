9b2795240d6b4f85eb91ebf31948b7082413b03d
Isaac Maamouche <maamouche.isaac@gmail.com>
2022-12-25 18:44:08 +0100

    chore: home page and accordion css cleanup

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,14 +3,9 @@ import React from "react";
 import "./home.scss";
 
 import hero_home from "../../images/hero_home.jpeg";
-import { Carousel } from "../../components/Carousel";
 import { HeroBanner } from "../../components/HeroBanner";
 import { data } from "../../constants/data";
-import { Accordion } from "../../components/Accordion";
 import { Card } from "../../components/Card";
-import { Tag } from "../../components/Tag";
-import { Rating } from "../../components/Rating";
-import { Host } from "../../components/Host";
 
 export const Home: React.FC = () => {
   return (
@@ -27,15 +22,6 @@ export const Home: React.FC = () => {
           />
         ))}
       </div>
-
-      <Host name={data[0].host.name} avatar={data[0].host.picture} />
-      <Rating rating={"4"} />
-      <Tag content="tag" />
-      <Card title="title" image={data[0].pictures[0]} link={"/#"} />
-      <Accordion heading="heading">
-        <p>content</p>
-      </Accordion>
-      <Carousel images={data[0].pictures} />
     </>
   );
 };
